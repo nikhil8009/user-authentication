3ac4b70913b37afa3e14dd072c7c9b7b36aa753e
Nikhil <nikhilbachhav91@gmail.com>
2024-02-28 21:34:31 +0530

    feat: Added docker configuration fix: Added update & delete for note

--- controllers/noteController.go
+++ controllers/noteController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -74,7 +75,74 @@ func GetNotes() gin.HandlerFunc {
 			return
 		}
 
-		c.JSON(http.StatusOK, notes)
+		c.JSON(http.StatusOK, gin.H{"data": notes})
 
 	}
+}
+
+func UpdateNote() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		noteId := c.Param("id")
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var note *models.Note
+
+		if err := c.BindJSON(&note); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+			defer cancel()
+			return
+		}
+
+		objectId, error := primitive.ObjectIDFromHex(noteId)
+		if error != nil {
+			log.Println(error.Error())
+			defer cancel()
+			return
+		}
+
+		jsonData := bson.M{}
+		if note.Title != nil {
+			jsonData["title"] = note.Title
+		}
+		if note.Description != nil {
+			jsonData["description"] = note.Description
+		}
+
+		err := noteCollection.FindOneAndUpdate(ctx, bson.M{"_id": objectId}, bson.M{"$set": jsonData})
+		defer cancel()
+		fmt.Println(err.Err())
+		if err.Err() != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Err()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"msg": "Note updated successfully"})
+	}
+}
+
+func DeleteNote() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		noteId := c.Param("id")
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		objectId, error := primitive.ObjectIDFromHex(noteId)
+		if error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": error.Error()})
+			defer cancel()
+			return
+		}
+
+		err := noteCollection.FindOneAndDelete(ctx, bson.M{"_id": objectId})
+		defer cancel()
+
+		if err.Err() != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Err()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"msg": "Note deleted successfully"})
+	}
+
 }
